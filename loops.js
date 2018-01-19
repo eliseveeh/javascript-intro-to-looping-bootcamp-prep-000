705797a3b7673107603b8b662648050edbbb65d2
Elizabeth Veeh <elise.veeh@gmail.com>
2018-01-19 18:28:32 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,12 +16,13 @@ function whileLoop(num){
   }
   return "done";
 }
+function maybeTrue() {
+  return Math.random() >= 0.5
+}  
+        
 function doWhileLoop(array){
-  function maybeTrue() {
-  Math.random() >= 0.5
-}
-    do {
-      array.pop();
-    }
-    while (array.length >= 0 && maybeTrue())
-}
+ do {
+  array.pop();
+} while (array.length>0 &&maybeTrue());               
+
+}     
